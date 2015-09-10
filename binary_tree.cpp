c43ac15339bceaf22e96524da9e171cd6b4be4a4
ZVampirEM <heroknightmm@163.com>
2015-09-10 19:57:26 +0800

    finish the intrust binarytree

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 #include <cstdlib>
+#include "binary_tree_traverse.h"
+
+using namespace std;
+
+int main()
+{
+	BinaryTree binarytree;
+	BTree *bt;
+	bt = (BTree*)binarytree.InstructBinaryTree();
+
+	return 0;
+}
